e559ba7878b773725f9fa1801b60aec0e4747e0f
Farid Ansari <farid12ansari7@gmail.com>
2018-10-08 00:29:49 +0530

    added compnay schema

--- schema/schema.js
+++ schema/schema.js
@@ -7,6 +7,15 @@ const  {
   GraphQLSchema,
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+})
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
